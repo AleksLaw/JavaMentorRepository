d4c97a39169aeaecd22bd776cc67035bb0ed255f
aleks <aleksandr.law@gmail.com>
2020-04-30 11:35:54 +0300

    start

---
+++ src/main/java/model/User.java
@@ -0,0 +1,55 @@
+package model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String surname;
+
+    public User(Long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return surname != null ? surname.equals(user.surname) : user.surname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + "id=" + id + ", name=" + name + ", surname=" + surname;
+    }
+}
